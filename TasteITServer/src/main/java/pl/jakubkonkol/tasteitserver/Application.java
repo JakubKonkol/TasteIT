66c7d25d8711049b330ca8f9049e17e614258621
kawcz <kmikolaj49@gmail.com>
2025-01-15 21:46:31 +0100

    not valid spring dockerization attempt

--- TasteITServer/src/main/java/pl/jakubkonkol/tasteitserver/Application.java
+++ TasteITServer/src/main/java/pl/jakubkonkol/tasteitserver/Application.java
@@ -2,10 +2,13 @@ package pl.jakubkonkol.tasteitserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = {MongoAutoConfiguration.class}
+)
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
